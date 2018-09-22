8bd98ca81b2519e4a2ce0989b81fd3b4f4f497b7
RobertDorobiala <42683580+RobertDorobiala@users.noreply.github.com>
2018-09-22 15:01:00 -0500

    Challenge Update

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+    public float Speed = 5f;
+    public GameObject MovingBlocker;
+    public GameObject EndPosition;
+    public GameObject StartPosition;
+    public Text WallCountText;
+
+    private int WallCount;
+
+    void Start()
+    {
+        WallCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        if (WallCount == 0)
+
+        {
+            transform.position = Vector3.MoveTowards(transform.position, EndPosition.transform.position, Speed * Time.deltaTime);
+            //WallCount = 1;
+        }
+            
+        //else if (WallCount == 1)
+
+        {
+            //transform.position = Vector3.MoveTowards(transform.position, StartPosition.transform.position, Speed * Time.deltaTime);
+            //WallCount = 0;
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,17 +7,22 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     public Text countText;
     public Text winText;
-
+    public Text pointsText;
+    public Text pickupText;
+    public GameObject NorthWall;
 
     private Rigidbody rb;
     private int count;
+    private int pickup;
 
-    void Start ()
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
-        SetCountText ();
+        pickup = 0;
+        SetCountText();
         winText.text = "";
+        pointsText.text = "";
     }
 
     void FixedUpdate()
@@ -31,24 +36,38 @@ public class PlayerController : MonoBehaviour {
             Application.Quit();
 
         rb.AddForce(movement * speed);
-    }
 
-    void OnTriggerEnter(Collider other)
+        if (pickup >= 12)
+        {
+            Destroy(NorthWall);
+        }
+    }
+void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
             count = count + 1;
+            pickup = pickup + 1;
             SetCountText ();
+        }
+        if (other.gameObject.CompareTag("Bad Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count - 1;
+            SetCountText();
         }
     }
 
     void SetCountText ()
     {
-        countText.text = "Count: " + count.ToString();
-        if (count >= 12)
+        countText.text = "Score: " + count.ToString();
+        pickupText.text = "Pickups: " + pickup.ToString();
+
+        if (pickup >= 20)
         {
             winText.text = "You Win!";
+            pointsText.text = "You Finished with a score of:" + count;
         }
     }
 }
